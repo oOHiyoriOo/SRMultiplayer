7f9b0d505e6a9f9d28537fde0644d1cbbe5a76d6
oOHiyoriOo <wolf.sf75@googlemail.com>
2020-03-02 16:17:09 +0100

    First Release + new icon

--- Main.py
+++ Main.py
@@ -1,5 +1,10 @@
-import sys
-import os
+# get admin rights
+import ctypes, sys
+##
+
+import subprocess
+import os, winshell
+
 import requests
 
 import urllib.request
@@ -7,15 +12,15 @@ import shutil
 import time
 
 from ZeroLogger.ZeroLogger import *
+from win32com.client import Dispatch
 
-# FileID = str("1s0iNX7UppwYNK3XrPQePu4J2jRURRw1H") #https://drive.google.com/file/d/1s0iNX7UppwYNK3XrPQePu4J2jRURRw1H/view?usp=sharing
-FileID = str("1qp0IYzh2Xlpp0eOTJTsulcsd0hnoQvLd") # testfile
+FileID = str("1s0iNX7UppwYNK3XrPQePu4J2jRURRw1H") #https://drive.google.com/file/d/1s0iNX7UppwYNK3XrPQePu4J2jRURRw1H/view?usp=sharing
 
 def initDownload():
     # get google downloader.
     # https://github.com/oOHiyoriOo/SRMultiplayer/raw/master/HostedFile/gget.exe
 
-    info("cheking dirs...")    
+    info("checking dirs...")    
     if not os.path.isdir('temp'):
         os.system('mkdir %cd%\\temp')
     
@@ -43,20 +48,54 @@ def clearUp():
     done_task("Deleted tools.")
 
 def MainDownload():
-    warn("Downloading SlimeRancher...")
+    warn("Downloading SlimeRancher... (This may take a while...)")
     GDriveGet(FileID,'./temp/SlimeRancher.rar')
-    warn("Downloading Multiplay...")
+    warn("Downloading Multiplayer...")
 
-    url = "https://github.com/oOHiyoriOo/SRMultiplayer/raw/master/HostedFile/Multiplayer.zip"
-    file_name = "./temp/Multiplayer.zip"
+    url = "https://github.com/oOHiyoriOo/SRMultiplayer/raw/master/HostedFile/Multiplayer.rar"
+    file_name = "./temp/Multiplayer.rar"
 
     with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
         shutil.copyfileobj(response, out_file)
 
 
 def UnPack():
-    os.system("%cd%/temp/unrar.exe x %cd%/temp/SlimeRancher.rar")
+    info("Unpacking Files... to C:\\Program Files")
 
+    os.system('%cd%/temp/unrar.exe x %cd%/temp/SlimeRancher.rar "C:\\Program Files"')
+    os.system("%cd%/temp/unrar.exe x %cd%/temp/Multiplayer.rar")
+    os.system("cls")
+    done_task("Extracted files.")
+    
+
+def installMultiplayer():
+    warn("Installing Multiplayer files!")
+    os.system('xcopy "%cd%\\Multiplayer\\*.*" "C:\\Program Files\\Slime.Rancher.v1.4.1c\\" /S /K /D /H /Y')
+    done_task("Done.")
+
+    info("Calling Injector. \n===================================")
+    injector = subprocess.Popen('"C:\\Program Files\\Slime.Rancher.v1.4.1c\\UnityInjector.exe"', shell=True)
+    time.sleep(10) # <-- sleep for 12''
+    injector.terminate() # <-- terminate the process
+    done_task("===================================\nInjector called")
+
+def createShortcut():
+    info("Create Desktop Shortcut!")
+    desktop = winshell.desktop()
+    path = os.path.join(desktop, "SlimeRancher Multiplayer.lnk")
+    target = r"C:\\Program Files\\Slime.Rancher.v1.4.1c\\SlimeRancher.exe"
+    wDir = r"C:\\Program Files\\Slime.Rancher.v1.4.1c\\"
+    icon = r"C:\\Program Files\\Slime.Rancher.v1.4.1c\\SlimeRancher.exe"
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(path)
+    shortcut.Targetpath = target
+    shortcut.WorkingDirectory = wDir
+    shortcut.IconLocation = icon
+    shortcut.save()
+
+    os.system("cls")
+    info("Everything should be done! hf ^^")
+    sys.exit(0)
 
 ##################################################################################################
 ##################################################################################################
@@ -94,12 +133,27 @@ def save_response_content(response, destination):
                 f.write(chunk)
 
 
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+
+
 ##################################################################################################
 ##################################################################################################
 ##################################################################################################
 
-initDownload()
 
-MainDownload()
+if is_admin():
+    initDownload()
 
-UnPack()
+    MainDownload()
+
+    UnPack()
+    installMultiplayer()
+    createShortcut()
+
+else:
+    ctypes.windll.shell32.ShellExecuteW(None, "runas",sys.executable, __file__,None, 1)
